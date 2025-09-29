444139defa99acaa1db792003b76a788e0002e0c
Avi <95044844+Avioti@users.noreply.github.com>
2025-09-29 11:26:46 -0400

    Made it into methods

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -4,31 +4,34 @@ import java.util.Scanner;
 
 public class FullNameApplication {
     public static void main(String[] args) {
-        Scanner scanner =  new Scanner(System.in);
-        System.out.println("Please enter your name");
-        System.out.print("First name: ");
-        String First_name = scanner.nextLine();
-        System.out.print("Middle name: ");
-        String Middle_name = scanner.nextLine();
-        System.out.print("Last name: ");
-        String Last_name = scanner.nextLine();
-        System.out.print("Suffix: ");
-        String Suffix = scanner.nextLine();
+
+
+Namecheck();
 
 
 
-
-        if (!Stringtruth(Middle_name)){
-            System.out.print(First_name.trim() + " " +  Middle_name.trim() + " " + Last_name.trim());
-        }else{
-            System.out.print(First_name.trim().concat(" ") + Middle_name.trim() + Last_name.trim());
-        }
-        if (!Stringtruth(Suffix)){
-            System.out.print(",".concat(" ") + Suffix.trim());
-        }
 
     }
-
+public static void Namecheck(){
+    Scanner scanner =  new Scanner(System.in);
+    System.out.println("Please enter your name");
+    System.out.print("First name: ");
+    String First_name = scanner.nextLine();
+    System.out.print("Middle name: ");
+    String Middle_name = scanner.nextLine();
+    System.out.print("Last name: ");
+    String Last_name = scanner.nextLine();
+    System.out.print("Suffix: ");
+    String Suffix = scanner.nextLine();
+    if (!Stringtruth(Middle_name)){
+        System.out.print(First_name.trim() + " " +  Middle_name.trim() + " " + Last_name.trim());
+    }else{
+        System.out.print(First_name.trim().concat(" ") + Middle_name.trim() + Last_name.trim());
+    }
+    if (!Stringtruth(Suffix)){
+        System.out.print(",".concat(" ") + Suffix.trim());
+    }
+}
     public static boolean Stringtruth(String str){
         if (str.trim().isEmpty())
             return true;
